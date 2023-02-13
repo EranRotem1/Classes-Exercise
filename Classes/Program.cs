379fb37c00ad2a76bf327d3834259f4f7b16ef22
Eran Rotem <eranrotem123@gmail.com>
2023-02-13 15:09:25 -0600

    exercise 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,24 @@
 ﻿namespace Classes
 {
-    public class Program
+    public class Car
     {
+        public Car(string _make, string _model, string _year)
+        {
+            Make = _make;
+            Model = _model;
+            Year = _year;   
+
+        }
+        public string Make;
+        public string Model;
+        public string Year;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car dreamCar = new Car("Audi", "R8", "2023");
+            Console.WriteLine(string.Join(" ", dreamCar.Make , dreamCar.Model , dreamCar.Year));
+            Console.WriteLine(dreamCar.Model);
+            Console.WriteLine(dreamCar.Year);
         }
     }
 }
